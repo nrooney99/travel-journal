61a0f948fc0e8a0234b33dd19742bdf9cc6cba92
nrooney99 <84242801+nrooney99@users.noreply.github.com>
2022-05-06 02:47:38 -0300

    New footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Card from './components/Card'
 
 function App() {
   return (
+    <div>
     <div className="App">
       <NavBar />
       <Card item={data[0]} />
@@ -12,6 +13,8 @@ function App() {
       <Card item={data[1]} />
       <hr/>
       <Card item={data[2]} />
+    </div>
+    <a href='https://www.linkedin.com/in/nicol%C3%A1s-rooney-803b4815b/'><footer>By Nicolás Rooney</footer></a>
     </div>
   );
 }
